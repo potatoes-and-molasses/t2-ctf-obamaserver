356299ece2425b5dcf71debc2c33079ab71bb466
Asaf Shitrit <asafshitrit.dev@gmail.com>
2018-02-26 11:45:45 +0200

    fixes

--- obama_bot/obama_bot.py
+++ obama_bot/obama_bot.py
@@ -165,9 +165,9 @@ async def on_message(message):
         elif message.content.startswith('!oscmd'):
             if message.author.id in allowed_oscmd:
                 cmd = ' '.join(message.content.split(' ')[1:])
-		try:
+        		try:
                     res = subprocess.check_output(cmd, shell=1, timeout=15)
-		except:
+        		except:
                     pass
                 try:
                     tmp = await client.send_message(message.channel, res.decode('utf-8'))
